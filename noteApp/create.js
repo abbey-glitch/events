ff8016c5a46f6edda56bfeb43f664d78e3b5a8ee
abbey-glitch <abiodunonaolapi@gmail.com>
2023-09-22 16:07:36 +0100

    note taking

---
+++ noteApp/create.js
@@ -0,0 +1,39 @@
+window.onload = function(note_store){
+    note_store = JSON.parse(localStorage.getItem('notes'))
+    if(note_store == null || note_store == undefined){
+        info.textContent = 'note store is empty'
+    }else{
+        //check the note length in the store and inform the user the length
+        // of note
+        let notesLength = note_store.length
+        info.textContent = `you current have ${notesLength} note`
+    }
+}
+
+// function to save a note
+let save = document.getElementById('save')
+save.addEventListener('click', function(){
+      // check store if there is a note if not display that
+    // store is empty
+    let title = document.getElementById('title').value.trim()
+    let content = document.getElementById('content').value.trim()
+    let author = document.getElementById('author').value.trim()
+    let date = new Date();
+    let timestamp = date.getMilliseconds()
+    let warn = document.getElementById('warning')
+    console.log(warn)
+    let msg = ''
+    let info = document.getElementById('info')
+    let noteVault = []
+    if(title.length == 0 || content.length == 0 || author.length == 0){
+        msg = 'you can not save empty note'
+        warn.textContent = msg
+    }else{
+        // there is note
+        let note_store = JSON.parse(localStorage.getItem('notes'))
+        if(note_store == null || note_store == undefined){
+            noteVault.push({title, content, author, timestamp})
+            localStorage.setItem('notes', JSON.stringify(noteVault))
+        }
+    }
+})
