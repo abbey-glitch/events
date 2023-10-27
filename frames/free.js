ce478332ccf2dabbb22b55ba4fa7a752c351e680
abbey-glitch <abiodunonaolapi@gmail.com>
2023-10-27 10:12:17 +0100

    bootstrap with jquery

---
+++ frames/free.js
@@ -0,0 +1,10 @@
+let grep = $("#exampleInputEmail1")[0]
+// console.log(grep)
+let list = $('li')
+$(function(){
+    $('li').each(function(){
+        let samid = this.id;
+        console.log(samid)
+    })
+    console.log(grep)
+});
---
+++ framework/my.js
@@ -0,0 +1,34 @@
+// // jquery syntax
+// let ders = $("#content").text()
+// console.log(ders)
+
+// // javascript 
+// let el1 = document.getElementById('content').innerText
+// console.log(el1)
+// let submit = document.getElementById('submit')
+// submit.addEventListener('click', function(){
+//     alert('hello')
+// })
+// let submit = $("#submit")
+// submit.on('click', function(){
+//     let username = $("#username").val()
+//     let pwd = $('#pwd').val()
+//     console.log(username, pwd)
+// })
+let cont = $(".container")
+console.log(cont)
+// var request = $.ajax({
+//     url: "script.php",
+//     method: "POST",
+//     data: { id : menuId },
+//     dataType: "html"
+//   });
+   
+//   request.done(function( msg ) {
+//     $( "#log" ).html( msg );
+//   });
+   
+//   request.fail(function( jqXHR, textStatus ) {
+//     alert( "Request failed: " + textStatus );
+//   });
+// console.log(submit)
